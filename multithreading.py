97526895e290f131ed80e6bbd113c50954bb800b
phpor <phpor.net@gmail.com>
2015-03-24 17:18:42 +0800

    add file

---
+++ multithreading.py
@@ -0,0 +1,12 @@
+import threading
+
+def work(x):
+    while x > 0:
+        print x
+        x = x - 1
+
+if __name__ == '__main__':
+    t = threading.Thread(target=work,args=(3,))
+    #t.setDaemon(True)
+    t.start()
+
