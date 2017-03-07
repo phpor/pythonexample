20642896b43bebb74c9320452c923db3a3658d6f
junjie.li <junjie.li@beebank.com>
2017-03-07 16:40:34 +0800

    学习使用paramico

---
+++ ssh.py
@@ -0,0 +1,16 @@
+import paramiko
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect('172.16.22.169', username='test', password='lijunjie')
+
+transport = client.get_transport()
+transport.set_keepalive(30)
+transport.use_compression(True)
+
+channel = transport.open_session()
+channel.set_environment_variable("SSH_TEST", "123")
+channel.invoke_shell()
+
+channel.close()
+client.close()
+
