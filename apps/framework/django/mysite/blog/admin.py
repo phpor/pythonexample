2e0198d4a556af44508505aa2cd8bd8477405796
junjie.li <junjie.li@beebank.com>
2017-08-28 17:25:57 +0800

    web框架学习

---
+++ apps/framework/django/mysite/blog/admin.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from models import Feed, User
+# Register your models here.
+
+admin.site.register(Feed)
+admin.site.register(User)
